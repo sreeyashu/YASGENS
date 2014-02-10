9c7bd1ea60591563719c5127e9a448e4e693e6cd
sreeyashu <sreeyashu@gmail.com>
2014-02-10 13:58:46 +0100

    Yash MR1 Latest

---
+++ js/custom.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	
+	$('.yashprint').on('click', function() {
+		window.print();
+	});
+	
+	 $('.tooltip').tooltipster();
+	$('.selected_details__account').on('click', function() {
+		$('.flyout_selection').toggleClass('flyout_active');
+		$('.selected_details__account.dropdown_arrow').toggleClass('dropdown-active');
+		$(".flyout_list_items").removeClass('show_listitems');
+		setTimeout(function () {
+			$(".flyout_list_items").addClass('show_listitems');
+		}, 300);
+		
+			
+	});
+		
+	$('.show_filters').on('click', function() {
+		  $('.filter_block').slideToggle();
+	 });
+	 $('.filter_period').on('click', function() {
+		  $('.option_period').toggleClass('show_active');
+	 });
+
+
+});
