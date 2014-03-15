765455de1ff6c1a779bc0f981a8d0f680f7edede
sreeyashu <sreeyashu@gmail.com>
2014-03-15 13:00:00 +0100

    yash

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,77 @@
 $(document).ready(function() {
+//ontouchstart="touchStart(event,'picture-frame');"  ontouchend="touchEnd(event);" ontouchmove="touchMove(event);" ontouchcancel="touchCancel(event);"
+var obj=$('body');
+alert("HII"+obj);
+$('.easybanking').on('touchmove',function(event){
+	alert("Hii22"+event.targetTouches.length);
+		
+		if (event.targetTouches.length == 1) {
+    var touch = event.targetTouches[0];
+    // Place element where the finger is
+	alert("touched at"+touch.pageX +"  "+touch.pageY );
+  }
+		
+		
+	});
+	
+
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+//var header = $('.site_header').outerHeight();
+//var moduleheader= $('.module_main_header').outerHeight();
+var navbarHeight = 0//$('.site_header').outerHeight();
+
+
+
+/*$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+*/
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+		$('.warning_area').css('top','0');
+        $('.site_header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+		//$('.module_header').css('top','80px');
+        if(st + $(window).height() < $(document).height()) {
+			$('.warning_area').css('top','80px');
+            $('.site_header').removeClass('nav-up').addClass('nav-down');
+			
+        }
+    }
+    lastScrollTop = st;
+}
+
+//var menu = document.querySelector('.module_header');
+//var origOffsetY = menu.offsetTop;
+//
+//function scroll () {
+//  if ($(window).scrollTop() >= origOffsetY) {
+//    $('.module_header').css('position','fixed').css('top','0px').css('width','790px');
+//    $('.content').addClass('menu-padding');
+//  } else {
+//    $('.module_header').css('position','relative').css('top','0px');
+//    $('.content').removeClass('menu-padding');
+//  }  
+//  
+//  
+//}
+//document.onscroll = scroll;
+
+
 	
 	$('input[type="text"]').focus(
     function(){
