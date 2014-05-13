b09af1293ad725f91fd89e61df64fd032fb5dcdc
sreeyashu <sreeyashu@gmail.com>
2014-05-13 11:06:45 +0200

    yash

--- js/stubs/fake-server.js
+++
@@ -1,6 +0,0 @@
-(function(){
-	//sinon.FakeXMLHttpRequest.useFilters = true;
-	var server = sinon.fakeServer.create();
-	server.autoRespond = true;
-	window.server = server;
-})();
--- js/stubs/sepa.js
+++
@@ -1,9 +0,0 @@
-define(['stubs/data/sepa'], function(SepaData){
-	window.server.respondWith(
-    /\/rpc\/sepa\/getFundTransferEntryCharacteristics/, [200, {
-            'Content-Type': 'application/json'
-        },
-        JSON.stringify(SepaData.getFundTransferEntryCharacteristics)]
-    );
-    return
-});
